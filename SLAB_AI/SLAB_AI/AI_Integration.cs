e4a3a57b4b1a93f943ec391ee1acb025976f6fcf
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-06-08 03:17:06 +0000

    Constructor and IK setter methods created but not implemented

--- SLAB_AI/SLAB_AI/AI_Integration.cs
+++ SLAB_AI/SLAB_AI/AI_Integration.cs
@@ -8,5 +8,23 @@ namespace SLAB_AI
 {
     public class AI_Integration
     {
+        /// <summary>
+        /// Main constructor for the library project.
+        /// </summary>
+        /// <param name="IK">Instrumentation key to be used.</param>
+        public AI_Integration(String IK)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Instrumentation key setter, used if user wants to alternate IKs without creating new objects.
+        /// </summary>
+        /// <param name="IK">Instrumentation Key to set, passed as parameter</param>
+        public void setInstrumentationKey(String IK)
+        {
+
+        }
     }
 }
