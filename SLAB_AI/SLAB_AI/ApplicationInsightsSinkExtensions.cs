585b2d5f1313a3952b85e8f69f95f1038dd209d5
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-07-08 19:01:58 -0600

    ApplicationInsightsSinkExtensions updated to allow the user to not send an IKey if is setted in the ApplicationInisigths.config otherwise it returns an exeption.

--- SLAB_AI/SLAB_AI/ApplicationInsightsSinkExtensions.cs
+++ SLAB_AI/SLAB_AI/ApplicationInsightsSinkExtensions.cs
@@ -27,6 +27,20 @@ namespace Microsoft.Practices.EnterpriseLibrary.SemanticLogging
         }
 
         /// <summary>
+        /// Creates an event listener that logs using a <see cref="ApplicationInsightsSink" />.
+        /// </summary>
+        /// <param name="TelemetryInitializer">The (optional) Application Insights telemetry initializers.</param>
+        /// <returns>
+        /// An event listener that uses <see cref="ApplicationInsightsSink" /> to log events.
+        /// </returns>
+        public static EventListener CreateListener(params ITelemetryInitializer[] telemetryInitializers)
+        {
+            var listener = new ObservableEventListener();
+            listener.LogToApplicationInsights(telemetryInitializers);
+            return listener;
+        }
+
+        /// <summary>
         /// Subscribes to an <see cref="IObservable{EventEntry}" /> using a <see cref="ApplicationInsightsSink" />.
         /// </summary>
         /// <param name="eventStream">The event stream. Typically this is an instance of <see cref="ObservableEventListener" />.</param>
@@ -37,6 +51,21 @@ namespace Microsoft.Practices.EnterpriseLibrary.SemanticLogging
         /// </returns>
         public static SinkSubscription<ApplicationInsightsSink> LogToApplicationInsights(this IObservable<EventEntry> eventStream,
       String InstrumentationKey, params ITelemetryInitializer[] telemetryInitializers)
+        {
+            var sink = new ApplicationInsightsSink(telemetryInitializers);
+            var subscription = eventStream.Subscribe(sink);
+            return new SinkSubscription<ApplicationInsightsSink>(subscription, sink);
+        }
+
+        /// <summary>
+        /// Subscribes to an <see cref="IObservable{EventEntry}" /> using a <see cref="ApplicationInsightsSink" />.
+        /// </summary>
+        /// <param name="eventStream">The event stream. Typically this is an instance of <see cref="ObservableEventListener" />.</param>
+        /// <param name="telemetryInitializers">The (optional) Application Insights telemetry initializers.</param>
+        /// <returns>
+        /// A subscription to the sink that can be disposed to unsubscribe the sink and dispose it, or to get access to the sink instance.
+        /// </returns>
+        public static SinkSubscription<ApplicationInsightsSink> LogToApplicationInsights(this IObservable<EventEntry> eventStream, params ITelemetryInitializer[] telemetryInitializers)
         {
             var sink = new ApplicationInsightsSink(telemetryInitializers);
             var subscription = eventStream.Subscribe(sink);
